57afcd836e44fb43f0dc87d6de9b25906622fc88
JackRaycoon <jack.o.raycoon@gmail.com>
2025-04-12 15:16:45 +0700

    Начало работы над стартовыми ивентами (Воин)

--- Assets/Scripts/Run DATA/Events/EventData.cs
+++ Assets/Scripts/Run DATA/Events/EventData.cs
@@ -2,13 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventData
+[CreateAssetMenu(fileName = "EventData", menuName = "Event Data", order = 4)]
+public class EventData : ScriptableObject
 {
-    public enum EventType
-   {
-      StartEvent,
-      EnteranceEvent
-   }
-   public bool isLockableEvent = false; //Ѕлокирует ли этот ивент выход из комнаты до его окончани€
+   [Tooltip("√лобальный номер самого ивента, у всех частей он один")]
+   public int eventID;
+
+   [Tooltip("Ќомер части ивента, формат: 1.1 или 120.4, первое число - глобальный номер, второе - номер части")]
+   public int eventID_Part;
+
+   [Tooltip("¬се части ивента имеют одно название, но могут дополн€тьс€, например: Ќайденное сокровище и Ќайденное сокровище: провал")]
+   [TextArea]
+   public string eventName;
+
+   [TextArea]
+   public string eventText;
+
+   [Tooltip("StartEvent - —тартовый дл€ персонажа, сюжетный?\n" +
+      "EnteranceEvent - ѕо€вл€етс€ после входа в комнату, старт ивента\n" +
+      "EventPart - „асть ивента, по сути после любого действи€ с ивентом мы переходим к его следующей части")]
    public EventType eventType;
+
+   [Tooltip("Ѕлокирует ли этот ивент выход из комнаты до его окончани€")]
+   public bool isLockableEvent = false;
+
+   [Tooltip("“екст на кнопку выбора, если это часть ивента")]
+   [TextArea]
+   public string textChoice;
+
+   [Tooltip("¬ыборы дл€ ивента, если пустой значит ивент закончилс€")]
+   public List<EventData> choices;
+
+   public enum EventType
+   {
+      EventPart, //„асть ивента, по сути после любого действи€ с ивентом мы переходим к его следующей части
+      StartEvent, //—тартовый дл€ персонажа, сюжетный?
+      EnteranceEvent //ѕо€вл€етс€ после входа в комнату, старт ивента
+   }
 }
