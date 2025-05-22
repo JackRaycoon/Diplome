a73ff21c9382d0f4313c8c094800c6554e769db7
JackRaycoon <jack.o.raycoon@gmail.com>
2025-05-22 18:58:34 +0700

    Добавил аудио файлы

--- Assets/Scripts/UI/SFXPlayer.cs
+++ Assets/Scripts/UI/SFXPlayer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SFXPlayer : MonoBehaviour
 {
    private AudioSource audioSource;
-   public AudioClip clickSound, sweepSound;
+   public AudioClip clickSound, sweepSound, scrollSound, doorSound, lockDoorSound;
 
    private void Awake()
    {
@@ -12,4 +12,7 @@ public class SFXPlayer : MonoBehaviour
 
    public void PlayClick() => audioSource.PlayOneShot(clickSound);
    public void PlaySweep() => audioSource.PlayOneShot(sweepSound);
+   public void PlayScroll() => audioSource.PlayOneShot(sweepSound);
+   public void PlayDoor() => audioSource.PlayOneShot(sweepSound);
+   public void PlayLockDoor() => audioSource.PlayOneShot(sweepSound);
 }
